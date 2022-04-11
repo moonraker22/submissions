faf1c045e24db138076421a60b36a86a9d04d7f2
moonraker22 <zxkst2@protonmail.com>
2022-04-11 05:24:37 -0500

    step 3&$

--- part1/unicafe/unicafe/src/components/statistics/Statistics.js
+++ part1/unicafe/unicafe/src/components/statistics/Statistics.js
@@ -1,21 +1,29 @@
 const Statistics = (props) => {
   const { good, neutral, bad } = props;
+  const totalFeedback = good + neutral + bad;
+  const average = (good - bad) / totalFeedback;
+  const positive = (good / totalFeedback) * 100;
 
-  return (
-    <>
-      <h2>Statistics</h2>
-      <br />
-      {good ? <p>Good: {good}</p> : null}
-      {neutral ? <p>Neutral: {neutral}</p> : null}
-      {bad ? <p>Bad: {bad}</p> : null}
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good + neutral + bad}</p>
-      <p>Average: {(good + neutral + bad) / 3}</p>
-      <p>Positive: {(good / (good + neutral + bad)) * 100} %</p>
-    </>
-  );
+  if (totalFeedback === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <br />
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {totalFeedback}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive} %</p>
+      </>
+    );
+  }
 };
 
 export default Statistics;
